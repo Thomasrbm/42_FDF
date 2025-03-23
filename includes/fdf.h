f90a9a2cad828afa4017aa76d66c8a9b3820c6c1
throbert <throbert@student.42.fr>
2025-03-23 16:17:57 +0100

    Makefile

--- includes/fdf.h
+++ includes/fdf.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   fdf.h                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: throbert <throbert@student.42lehavre.fr>   +#+  +:+       +#+        */
+/*   By: throbert <throbert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/08 07:01:48 by throbert          #+#    #+#             */
-/*   Updated: 2025/02/10 05:02:28 by throbert         ###   ########.fr       */
+/*   Updated: 2025/03/23 16:01:17 by throbert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 # define FDF_H
 
 # include "../minilibx-linux/mlx.h"
-# include "get_next_line.h"
+# include "../gnl/get_next_line.h"
 # include "../libft/libft.h"
 # include <fcntl.h>
 # include <limits.h>
